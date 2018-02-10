3a30ad29e87572ff93b96725eedd3445ea1c3ceb
Kashif Ahmad <kashikushi@gmail.com>
2018-02-10 08:41:09 +0700

    first commit from DreamWeaver CC

---
+++ index.php
@@ -0,0 +1,323 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Kdemy</title>
+<!-- Meta tag Keywords -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Education portal Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--// Meta tag Keywords -->
+<!-- css files -->
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all">
+<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
+<link href="css/carousel.css" type="text/css" rel="stylesheet" media="all">
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all">
+<!-- gallery -->
+<link href="css/lsb.css" rel="stylesheet" type="text/css">
+<!-- //gallery -->
+<!-- /fonts -->
+<link href="//fonts.googleapis.com/css?family=Montserrat+Alternates:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic" rel="stylesheet">
+<!-- //fonts -->
+
+
+
+
+<!-- //css files -->
+<!-- js -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<script type="text/javascript" src="js/bootstrap-3.1.1.min.js"></script>
+
+<!-- //js -->
+</head>
+<body>
+<?php 
+
+include("header.html");
+
+?>
+
+
+<!-- banner -->
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel" data-slide-to="1" class=""></li>
+			<li data-target="#myCarousel" data-slide-to="2" class=""></li>
+			<li data-target="#myCarousel" data-slide-to="3" class=""></li>
+			<li data-target="#myCarousel" data-slide-to="4" class=""></li>
+		</ol>
+		<div class="carousel-inner" role="listbox">
+			<div class="item active">
+				<div class="container">
+					<div class="carousel-caption">
+						<h2>Bringing education to life</h2>
+						<p>In this digital age, young people need to be highly skilled in the use of ICT. While schools already employ these technologies in learning, there is a need to increase their effectiveness significantly over the next decade.</p>
+						<button class="btn btn-primary" data-target="#myModal" data-toggle="modal">Read more</button>
+					</div>
+				</div>
+			</div>
+			<div class="item item2">
+				<div class="container">
+					<div class="carousel-caption">
+						<h3>How to deal with exam stress</h3>
+						<p>Exam season can bring on levels of stress and burnout that can hinder your studies. Here are some handy tips on how to manage your anxiety.</p>
+						<button class="btn btn-primary" data-target="#myModal" data-toggle="modal">Now Open</button>
+					</div>
+				</div>
+			</div>
+			<div class="item item3">
+				<div class="container">
+					<div class="carousel-caption">
+						<h3>Teacher working with students</h3>
+						<p>Special education teachers design and deliver lessons geared towards the individual needs and capabilities of the students under their supervision.</p>
+						<button class="btn btn-primary" data-target="#myModal" data-toggle="modal">Now Open</button>
+					</div>
+				</div>
+			</div>
+			<div class="item item4">
+				<div class="container">
+					<div class="carousel-caption">
+						<h3>Learn and Grow</h3>
+						<p>If you want your child to build a rocket and see how high it flies, construct a robot out of Legos, make a fake fossil, bake their own granola and a thousand other things, then Learn and Grow is for you!</p>
+						<button class="btn btn-primary" data-target="#myModal" data-toggle="modal">Now Open</button>
+					</div>
+				</div>
+			</div>
+			<div class="item item5">
+				<div class="container">
+					<div class="carousel-caption">
+						<h3>INSPIRE. ENGAGE. TRANSFORM.</h3>
+						<p>Kdemy is a social impact consulting firm offering comprehensive strategy and communications consultation services.</p>
+						<button class="btn btn-primary" data-target="#myModal" data-toggle="modal">Now Open</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+		<!-- The Modal -->
+		<div id="myModal" class="modal wthree-modal" tabindex="-1">
+			<!-- Modal content -->
+			<div class="modal-content">
+				<div class="modal-header">
+					<span class="close" data-dismiss="modal" >&times;</span>
+					<h3>Kdemy</h3>
+				</div>
+				<div class="col-md-6 modal-img">
+					<img src="images2/banner8.jpg" class="img-responsive" alt="w3layouts" title="w3layouts">
+				</div>
+				<div class="col-md-6 modal-text">
+					<p class="banner-p1">Kdemy brings broad experience and deep content knowledge to every aspect of our work. Effective planning, efficiency, creativity, and style are infused into each of our projects — always with an eye toward achieving results.</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+		<div class="thim-click-to-bottom">
+			<a href="#admission" class="scroll">
+				<i class="fa  fa-chevron-down"></i>
+			</a>
+		</div>
+    </div>
+	<!-- //banner -->
+
+</section>
+
+<!--//header-banner-section-end-here -->
+<!--/experience overview -->
+<div class="w3-faculty-ex">
+	<div class="w3-agile-faculty-head">
+	<h3>Our overview</h3>
+	</div>
+	<div class="w3-aglile-faculty-grids">
+			<div class="w3-aglile_faculty-grid1">
+					<div class="col-md-3 w3-faculty-img1">
+					</div>
+					<div class="col-md-3 w3-faculty-data">
+						<h3>Faculty Experience</h3>
+						 <ul>
+							 <li><span><i class="fa fa-check" aria-hidden="true"></i></span>Quality improvement</li>
+							 <li><span><i class="fa fa-check" aria-hidden="true"></i></span>Service effectiveness</li>
+							 <li><span><i class="fa fa-check" aria-hidden="true"></i></span>Vocational education</li>
+							 <li><span><i class="fa fa-check" aria-hidden="true"></i></span>Training efficiency</li>
+							 <li><span><i class="fa fa-check" aria-hidden="true"></i></span>Consultation services</li>
+						 </ul>
+					</div>
+					<div class="col-md-3 w3-faculty-ex-years">
+						<i class="fa fa-calendar-plus-o" aria-hidden="true"></i>
+						<h3>15</h3>
+						<p>more than 15 years of experience </p>
+					</div>
+					<div class="col-md-3 w3-faculty-img2">
+					</div>
+					<div class="clearfix"></div>
+			</div>
+			<div class="w3-agile-faculty-grid2">
+			<div class="col-md-3 w3-faculty-img3">
+					</div>
+				<div class="col-md-3 w3-faculty-ex-years">
+						<i class="fa fa-users" aria-hidden="true"></i>
+						<h3>20</h3>
+						<p>more than 20 years of experience </p>
+				</div>
+				<div class="col-md-3 w3-faculty-data">
+						<h3>Female Faculty</h3>
+						 <ul>
+							 <li><span><i class="fa fa-check" aria-hidden="true"></i></span>Someone who cares</li>
+							 <li><span><i class="fa fa-check" aria-hidden="true"></i></span>Not being alone</li>
+							 <li><span><i class="fa fa-check" aria-hidden="true"></i></span>Affirming one's worth</li>
+							 <li><span><i class="fa fa-check" aria-hidden="true"></i></span>Acts in best interest</li>
+							 <li><span><i class="fa fa-check" aria-hidden="true"></i></span>Feeling of connection</li>
+						 </ul>
+				</div>
+				<div class="col-md-3 w3-faculty-img4">
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="w3-agile-faculty-grid3">
+				<div class="col-md-4 w3-faculty-services1">
+							<i class="fa fa-user" aria-hidden="true"></i>
+						<h3>24*7 faculty</h3>
+						<p>Kdemy helps you get the most out of your investment through a wide range of education offerings.</p>
+				</div>
+				<div class="col-md-4 w3-faculty-services2">
+						<i class="fa fa-clock-o" aria-hidden="true"></i>
+						<h3>24 hours labs</h3>
+						<p>This place is awesome, especially for anyone that lives on campus. Scan your ID Card in order to get in.</p>
+				</div>
+				<div class="col-md-4 w3-faculty-services3">
+						<i class="fa fa-desktop" aria-hidden="true"></i>
+						<h3>Online training</h3>
+						<p>Kdemy is an online learning and teaching marketplace with over 65000 courses and 15 million students.</p>
+				</div>
+
+				<div class="clearfix"></div>
+			</div>
+
+
+	</div>
+	<div class="clearfix"></div>
+</div>
+<!--//experience overview -->
+
+		<!-- professors -->
+	<div class="jarallax team" id="professor">
+		<div class="team-dot">
+			<div class="container">
+				<div class="w3-agile-heading team-heading">
+					<h3>Our Trainers</h3>
+				</div>
+				<div class="agileits-team-grids">
+					<div class="col-md-3 agileits-team-grid">
+						<div class="team-info">
+							<img src="images2/teacher1.jpg" alt="">
+							<div class="team-caption">
+								<h4>Leila Knight </h4>
+								<p>Associate Lecturer</p>
+								<ul>
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-rss"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 agileits-team-grid">
+						<div class="team-info">
+							<img src="images2/teacher2.jpg" alt="">
+							<div class="team-caption">
+								<h4>Greville Henwood</h4>
+								<p>Professor</p>
+								<ul>
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-rss"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 agileits-team-grid">
+						<div class="team-info">
+							<img src="images2/teacher4.jpg" alt="">
+							<div class="team-caption">
+								<h4>Lana Hanssens</h4>
+								<p>Teacher/Trainer</p>
+								<ul>
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-rss"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 agileits-team-grid">
+						<div class="team-info">
+							<img src="images2/teacher5.jpg" alt="">
+							<div class="team-caption">
+								<h4>Daphne Sharpe</h4>
+								<p>Skill Trainer</p>
+								<ul>
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-rss"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+		</div>
+	</div>		
+	<!-- //professor -->
+	<!-- footer -->
+<?php 
+
+include("footer.html");
+
+?>
+	<!-- //footer -->
+
+	<!-- start-smooth-scrolling -->
+	<script type="text/javascript" src="js/move-top.js"></script>
+	<script type="text/javascript" src="js/easing.js"></script>
+	<script type="text/javascript">
+			jQuery(document).ready(function($) {
+				$(".scroll").click(function(event){
+					event.preventDefault();
+
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+				});
+			});
+	</script>
+	<!-- //end-smooth-scrolling -->
+	<!-- smooth-scrolling-of-move-up -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+			var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear'
+			};
+			*/
+
+			$().UItoTop({ easingType: 'easeOutQuart' });
+
+		});
+	</script>
+	<!-- //smooth-scrolling-of-move-up -->
+    </body>
+</html>
