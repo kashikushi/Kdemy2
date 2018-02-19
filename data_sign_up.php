50611d875a7f86622c955233a4b8e95b69ecf921
Kashif Ahmad <kashikushi@gmail.com>
2018-02-20 03:39:52 +0700

    sign up form, database connection code updated.

--- data_sign_up.php
+++ data_sign_up.php
@@ -1,40 +1,29 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>data</title>
-</head>
+<?php
+$con=mysqli_connect("localhost","root","root","dw_education");
+// Check connection
+define('USE_PCONNECT', 'false'); 
+define('STORE_SESSIONS', 'mysql');
+if (mysqli_connect_errno()) {
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ }
 
-<body>
-<!--db POST start
--->
 
-<?php	
-/*	db connection
-*/	
-	include("db_con.php"); 
+if(isset($_POST['submit']))
+ {
+ extract($_POST);
 
-/*data from signup page
-*/	
-	if (isset($_POST['submit']))
-	{
-		extract($_POST);
-			$userEmail = $_REQUEST['email'];
-			$userPassword = $_REQUEST['password'];
 
-			$query="INSERT INTO `Sign_Up_Data_Table`(`email_address`, `password`) VALUES ([userEmail],[userPassword])";
-	}
-	if ($con->query($query)) === TRUE) {
-		header("location:login.php");			
-	}
-	else {
-		echo "Error: " . $query . "<br>" . $con->error;
-	}
-	echo "<script> alert ('Recod has been submitted successfully.'); </script>";
-	
-	
-?>
-<!--db POST end
--->	
-</body>
-</html>
+
+$query="INSERT INTO `test1`(`email`, `password`) VALUES ('$useremail','$userpassword')";
+
+if ($con->query($query) === TRUE) {
+header("location:Sign_Up.php");
+
+ } else {
+echo "Error: " . $query . "<br>" . $con->error;
+}
+echo "<script> alert('Record Has been Recorded successfully.');</script>";
+/*$con->close();*/
+ }
+
+ ?>
--- db_con.php
+++ db_con.php
@@ -1,30 +1,5 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>db connection</title>
-</head>
-
-<body>
-
-/*db connention start
-*/
 
 <?php	
 	$con=mysqli_connect("sql303.epizy.com", "epiz_21599476", "yg5dquhElOrp", "epiz_21599476_dw_education")
-		// Check Connection
-		define('USE_PCONNECT', 'false');
-		define('STORE_SESSIONS', 'mysql');
-	if (mysqli_connect_errno()) {
-		echo "Failed to connect to MYSQL: " . mysqli_connect_error();
-	}	
+		
 ?>
-/*db connention end
-*/	
-	
-	
-	
-
-
-</body>
-</html>
