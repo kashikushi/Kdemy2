3e5f57d41ac2ab20073ae36df0f87c98b5e651ae
Kashif Ahmad <kashikushi@gmail.com>
2018-02-21 19:21:39 +0700

    sign_up page connected with db.

--- data_sign_up.php
+++ data_sign_up.php
@@ -1,6 +1,6 @@
 <?php
 
-$con=mysqli_connect("localhost","root","root","dw_education");
+$con=mysqli_connect("localhost","root","","dw_education");
 
 // Check connection
 define('USE_PCONNECT', 'false'); 
@@ -15,9 +15,11 @@ if(isset($_POST['submit']))
 
 $useremail = $_REQUEST['email'];
 $userpassword = $_REQUEST['password'];
+$userconfirmedpassword = $_REQUEST['password_confirmation'];
+
 
 	 
-$query="INSERT INTO `test1`(`email`, `password`) VALUES ('$useremail','$userpassword')";
+$query="INSERT INTO `Sign_Up_Data_Table_2`(`email_address`, `password`, `confirmed_password`) VALUES ('$useremail','$userpassword','$userconfirmedpassword')";
 
 if ($con->query($query) === TRUE) {
 header("location:Sign_Up.php");
